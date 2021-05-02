1ad9def77b806784707b04f2acd36ef2d4a2dc75
zaim-afk <82597006+zaim-afk@users.noreply.github.com>
2021-05-02 14:44:55 +0300

    Update MyProject.java

--- MyProject.java
+++ MyProject.java
@@ -1,10 +1,38 @@
+import java.io.*;
+import java.util.*;
+ 
 // Nahian Hasan Zaim (22849901)
 // Wai Yee Goh (22769803)
 
 public class MyProject implements Project {
+
+    
     public boolean allDevicesConnected(int[][] adjlist) {
         // TODO
-        return false;
+
+        boolean visited[] = new boolean[adjlist.length];
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        visited[0] = true;
+        queue.add(0);
+
+        while (queue.size() != 0) {
+            int u = queue.poll();
+
+            for (int col = 0; col < adjlist[u].length; col++) {
+                int v = adjlist[u][col];
+                
+                if(!visited[v]){
+                    visited[v] = true;
+                    queue.add(v);
+                }
+            }
+        }
+
+        for(boolean b : visited) if(!b) return false;
+            return true;
+
     }
 
     public int numPaths(int[][] adjlist, int src, int dst) {
